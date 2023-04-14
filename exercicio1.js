19b42fd5189a75a252deefad13ecc67eda16857c
Luiz <luizgustavo159753@gmail.com>
2023-04-14 10:11:32 -0400

    exercicios finalizados

--- exercicio1.js
+++ exercicio1.js
@@ -1,17 +1,17 @@
 
 let ValorCompra = 200;
-let parcelas = 6;
+let parcelas = 1;
 let desconto = 10;
 let ValorTotal;
 
 if(parcelas === 1){
-    ValorTotal = ValorCompra * (desconto/100);
+    ValorTotal = (ValorCompra - (ValorCompra * (desconto/100))).toFixed(2);
 
-    console.log("O valor a pagar é : $" + ValorTotal + ", pois à VISTA voçê ganhou 10% de  desconto.");
+    console.log("O valor a pagar é : R$" + ValorTotal + ", pois à VISTA voçê ganhou 10% de  desconto.");
 }
 else{
-    ValorTotal = ValorCompra / parcelas;
+    ValorTotal = (ValorCompra / parcelas).toFixed(2);
 
-    console.log("O valor da parcela é : $" + ValorTotal);
+    console.log(`Voce pagará ${parcelas} parcelas no valor de: R$${ValorTotal} (sem juros)`);
 }
 
